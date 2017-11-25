014bdae02bdb9f72e26b257aaa038b6014baf020
marisalom <ttonmaii@gmail.com>
2017-11-25 17:05:21 +0700

    TB_UserLogin SQLiteDatabase

---
+++ app/src/main/java/Model/TBUserLoginModel.java
@@ -0,0 +1,98 @@
+package Model;
+
+/**
+ * Created by marisalom on 25/11/2017.
+ */
+
+public class TBUserLoginModel {
+    private String ulUserLoginId;
+    private String ulName;
+    private String ulPass;
+    private String ulDesc;
+    private String ulGroupId;
+    private String ulStatus;
+    private String ulEmployeeId;
+    private String ulBranchId;
+    private String ulSetPermission;
+    private String ulRemoteAddr;
+
+    public String getUlUserLoginId() {
+        return ulUserLoginId;
+    }
+
+    public void setUlUserLoginId(String ulUserLoginId) {
+        this.ulUserLoginId = ulUserLoginId;
+    }
+
+    public String getUlName() {
+        return ulName;
+    }
+
+    public void setUlName(String ulName) {
+        this.ulName = ulName;
+    }
+
+    public String getUlPass() {
+        return ulPass;
+    }
+
+    public void setUlPass(String ulPass) {
+        this.ulPass = ulPass;
+    }
+
+    public String getUlDesc() {
+        return ulDesc;
+    }
+
+    public void setUlDesc(String ulDesc) {
+        this.ulDesc = ulDesc;
+    }
+
+    public String getUlGroupId() {
+        return ulGroupId;
+    }
+
+    public void setUlGroupId(String ulGroupId) {
+        this.ulGroupId = ulGroupId;
+    }
+
+    public String getUlStatus() {
+        return ulStatus;
+    }
+
+    public void setUlStatus(String ulStatus) {
+        this.ulStatus = ulStatus;
+    }
+
+    public String getUlEmployeeId() {
+        return ulEmployeeId;
+    }
+
+    public void setUlEmployeeId(String ulEmployeeId) {
+        this.ulEmployeeId = ulEmployeeId;
+    }
+
+    public String getUlBranchId() {
+        return ulBranchId;
+    }
+
+    public void setUlBranchId(String ulBranchId) {
+        this.ulBranchId = ulBranchId;
+    }
+
+    public String getUlSetPermission() {
+        return ulSetPermission;
+    }
+
+    public void setUlSetPermission(String ulSetPermission) {
+        this.ulSetPermission = ulSetPermission;
+    }
+
+    public String getUlRemoteAddr() {
+        return ulRemoteAddr;
+    }
+
+    public void setUlRemoteAddr(String ulRemoteAddr) {
+        this.ulRemoteAddr = ulRemoteAddr;
+    }
+}
---
+++ app/src/main/java/com/example/ton/furnitureapplication/DatabaseHelper.java
@@ -0,0 +1,145 @@
+package com.example.ton.furnitureapplication;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import Model.TBUserLoginModel;
+
+/**
+ * Created by marisalom on 25/11/2017.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private SQLiteDatabase database;
+    private static final String DATABASE_NAME = "SQLiteDatabase.db";
+    public static final int DATABASE_VERSION = 1;
+    public static final String TABLE_USERLOGIN = "TBUserLogin";
+
+    public static final String COL_USERLOGINID = "ulUserLoginId";
+    public static final String COL_NAME = "ulName";
+    public static final String COL_PASS = "ulPass";
+    public static final String COL_DESC = "ulDesc";
+    public static final String COL_GROUPID = "ulGroupId";
+    public static final String COL_STATUS = "ulStatus";
+    public static final String COL_EMPLOYEEID = "ulEmployeeId";
+    public static final String COL_BRANCHID = "ulBranchId";
+    public static final String COL_SETPERMISSION = "ulSetPermission";
+    public static final String COL_REMOTEADDR = "ulRemoteAddr";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_USERLOGIN_TABLE = "CREATE TABLE " + TABLE_USERLOGIN + "("
+                + COL_USERLOGINID + " CHAR(10) PRIMARY KEY, "
+                + COL_NAME + " VARCHAR(20), "
+                + COL_PASS + " VARCHAR(80), "
+                + COL_DESC + " VARCHAR(50), "
+                + COL_GROUPID + " VARCHAR(10), "
+                + COL_STATUS + " CHAR(1), "
+                + COL_EMPLOYEEID + " VARCHAR(10), "
+                + COL_BRANCHID + " VARCHAR(10), "
+                + COL_SETPERMISSION + " CHAR(1), "
+                + COL_REMOTEADDR + " VARCHAR(15) "
+                + ")";
+        db.execSQL(CREATE_USERLOGIN_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " +TABLE_USERLOGIN);
+        onCreate(db);
+    }
+
+    public void insertRecord(TBUserLoginModel userLogin){
+        database = this.getReadableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_USERLOGINID, userLogin.getUlBranchId());
+        contentValues.put(COL_NAME, userLogin.getUlName());
+        contentValues.put(COL_PASS, userLogin.getUlPass());
+        contentValues.put(COL_DESC, userLogin.getUlDesc());
+        contentValues.put(COL_GROUPID, userLogin.getUlGroupId());
+        contentValues.put(COL_STATUS, userLogin.getUlStatus());
+        contentValues.put(COL_EMPLOYEEID, userLogin.getUlEmployeeId());
+        contentValues.put(COL_BRANCHID, userLogin.getUlBranchId());
+        contentValues.put(COL_SETPERMISSION, userLogin.getUlSetPermission());
+        contentValues.put(COL_REMOTEADDR, userLogin.getUlRemoteAddr());
+        database.insert(TABLE_USERLOGIN, null, contentValues);
+        database.close();
+    }
+
+    public void insertRecordAlternate(TBUserLoginModel userLogin) {
+        database = this.getReadableDatabase();
+        database.execSQL("INSERT INTO " + TABLE_USERLOGIN + "(" + COL_USERLOGINID + ","
+                + COL_NAME + ","
+                + COL_PASS + ","
+                + COL_DESC + ","
+                + COL_GROUPID + ","
+                + COL_STATUS + ","
+                + COL_EMPLOYEEID + ","
+                + COL_BRANCHID + ","
+                + COL_SETPERMISSION + ","
+                + COL_REMOTEADDR
+                + ") VALUES('"
+                + userLogin.getUlUserLoginId() + "','"
+                + userLogin.getUlName() + "','"
+                + userLogin.getUlPass() + "','"
+                + userLogin.getUlDesc() + "','"
+                + userLogin.getUlGroupId() + "','"
+                + userLogin.getUlStatus() + "','"
+                + userLogin.getUlEmployeeId() + "','"
+                + userLogin.getUlBranchId() + "','"
+                + userLogin.getUlSetPermission() + "','"
+                + userLogin.getUlRemoteAddr() + "')");
+        database.close();
+    }
+
+    public void updateRecord(TBUserLoginModel userLogin) {
+        database = this.getReadableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_NAME, userLogin.getUlName());
+        contentValues.put(COL_PASS, userLogin.getUlPass());
+        contentValues.put(COL_DESC, userLogin.getUlDesc());
+        contentValues.put(COL_GROUPID, userLogin.getUlGroupId());
+        contentValues.put(COL_STATUS, userLogin.getUlStatus());
+        contentValues.put(COL_EMPLOYEEID, userLogin.getUlEmployeeId());
+        contentValues.put(COL_BRANCHID, userLogin.getUlBranchId());
+        contentValues.put(COL_SETPERMISSION, userLogin.getUlSetPermission());
+        contentValues.put(COL_REMOTEADDR, userLogin.getUlRemoteAddr());
+        database.update(TABLE_USERLOGIN, contentValues, COL_USERLOGINID + " = ?", new String[]{userLogin.getUlUserLoginId()});
+        database.close();
+    }
+
+    public void updateRecordAlternate(TBUserLoginModel userLogin) {
+        database = this.getReadableDatabase();
+        database.execSQL("update " + TABLE_USERLOGIN + " set "
+                + COL_NAME + " = '" + userLogin.getUlName() + "', "
+                + COL_PASS + " = '" + userLogin.getUlPass() + "', "
+                + COL_DESC + " = '" + userLogin.getUlDesc() + "', "
+                + COL_GROUPID + " = '" + userLogin.getUlGroupId() + "', "
+                + COL_STATUS + " = '" + userLogin.getUlStatus() + "', "
+                + COL_EMPLOYEEID + " = '" + userLogin.getUlEmployeeId() + "', "
+                + COL_BRANCHID + " = '" + userLogin.getUlBranchId() + "', "
+                + COL_SETPERMISSION + " = '" + userLogin.getUlSetPermission() + "', "
+                + COL_REMOTEADDR + " = '" + userLogin.getUlRemoteAddr()
+                + "' where "
+                + COL_USERLOGINID + " = '" + userLogin.getUlUserLoginId() + "'");
+        database.close();
+    }
+
+    public void deleteRecord(TBUserLoginModel userLogin) {
+        database = this.getReadableDatabase();
+        database.delete(TABLE_USERLOGIN, COL_USERLOGINID + " = ?", new String[]{userLogin.getUlUserLoginId()});
+        database.close();
+    }
+
+    public void deleteRecordAlternate(TBUserLoginModel userLogin) {
+        database = this.getReadableDatabase();
+        database.execSQL("delete from " + TABLE_USERLOGIN + " where " + COL_USERLOGINID + " = '" + userLogin.getUlUserLoginId() + "'");
+        database.close();
+    }
+}
